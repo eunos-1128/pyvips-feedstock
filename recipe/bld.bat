54fc6c457cf273472fcbd81a16a327c172cb552c
Kleis Auke Wolthuizen <github@kleisauke.nl>
2026-05-29 23:32:20 +0200

    Use pkgconf instead of pkg-config

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,15 +3,10 @@ setlocal enabledelayedexpansion
 
 %PYTHON% %RECIPE_DIR%\make_lcms2_pc.py
 
-@REM Drop Unix-only libm from pkg-config-derived libraries on MSVC
-%PYTHON% %RECIPE_DIR%\patch_pyvips_build.py
-
-copy %LIBRARY_LIB%\bzip2.lib %LIBRARY_LIB%\bz2.lib 
-copy %LIBRARY_LIB%\liblzma.lib %LIBRARY_LIB%\lzma.lib
-copy %LIBRARY_LIB%\libcrypto.lib %LIBRARY_LIB%\crypto.lib
-copy %LIBRARY_LIB%\libssl.lib %LIBRARY_LIB%\ssl.lib
-copy %LIBRARY_LIB%\libcurl.lib %LIBRARY_LIB%\curl.lib
+@REM Switch default pkg-config implementation to pkgconf
+@REM https://github.com/conda-forge/conda-forge.github.io/issues/1880
+set PKG_CONFIG=pkgconf
 
 @REM Ensure we can find the vips package
-pkg-config --exists --print-errors vips
+pkgconf --exists --print-errors vips
 %PYTHON% -m pip install . --no-deps --ignore-installed -vv
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -exo pipefail
-  
+
+# Switch default pkg-config implementation to pkgconf
+# https://github.com/conda-forge/conda-forge.github.io/issues/1880
+export PKG_CONFIG=pkgconf
+
+# Allow pkg-config to find xproto.pc
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$PREFIX/share/pkgconfig"
+
 # Ensure we can find the vips package
-pkg-config --exists --print-errors vips
+pkgconf --exists --print-errors vips
 
 ${PYTHON} -m pip install . --no-deps --ignore-installed -vv
