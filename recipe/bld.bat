548dcd82aec3ecad52f149552542f2ec8864bd30
UENO, M. <eunosium-1128@live.jp>
2026-05-30 03:02:30 +0900

    Add copying of libcrypto and libssl libraries

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,8 @@ setlocal enabledelayedexpansion
 
 copy %LIBRARY_LIB%\bzip2.lib %LIBRARY_LIB%\bz2.lib 
 copy %LIBRARY_LIB%\liblzma.lib %LIBRARY_LIB%\lzma.lib
+copy %LIBRARY_LIB%\libcrypto.lib %LIBRARY_LIB%\crypto.lib
+copy %LIBRARY_LIB%\libssl.lib %LIBRARY_LIB%\ssl.lib
 :: if exist %LIBRARY_LIB%\libarchive.lib copy %LIBRARY_LIB%\libarchive.lib %LIBRARY_LIB%\archive.lib
 :: if exist %LIBRARY_LIB%\zlib.lib copy %LIBRARY_LIB%\zlib.lib %LIBRARY_LIB%\z.lib
 :: if exist %LIBRARY_LIB%\libbz2.lib copy %LIBRARY_LIB%\libbz2.lib %LIBRARY_LIB%\bz2.lib
