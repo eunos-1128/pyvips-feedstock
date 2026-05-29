2bed3caf5b26fe60176ba5f9f17e87a3cdd6e429
UENO, M. <eunosium-1128@live.jp>
2026-05-30 01:59:38 +0900

    Update bld.bat to include libzstd and vips check
    Added check for libzstd.lib and ensured vips package availability.

---
+++ recipe/bld.bat
@@ -0,0 +1,16 @@
+@echo on
+setlocal enabledelayedexpansion
+
+%PYTHON% %RECIPE_DIR%\make_lcms2_pc.py
+
+dir %LIBRARY_LIB%\*.lib
+if exist %LIBRARY_LIB%\libarchive.lib copy %LIBRARY_LIB%\libarchive.lib %LIBRARY_LIB%\archive.lib
+if exist %LIBRARY_LIB%\zlib.lib copy %LIBRARY_LIB%\zlib.lib %LIBRARY_LIB%\z.lib
+if exist %LIBRARY_LIB%\libbz2.lib copy %LIBRARY_LIB%\libbz2.lib %LIBRARY_LIB%\bz2.lib
+if exist %LIBRARY_LIB%\liblzma.lib copy %LIBRARY_LIB%\liblzma.lib %LIBRARY_LIB%\lzma.lib
+if exist %LIBRARY_LIB%\liblz4.lib copy %LIBRARY_LIB%\liblz4.lib %LIBRARY_LIB%\lz4.lib
+if exist %LIBRARY_LIB%\libzstd.lib copy %LIBRARY_LIB%\libzstd.lib %LIBRARY_LIB%\zstd.lib
+
+@REM Ensure we can find the vips package
+pkg-config --exists --print-errors vips
+%PYTHON% -m pip install . --no-deps --ignore-installed -vv
