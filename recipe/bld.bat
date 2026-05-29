471d5b9f0ddffab8bd0ddd08102cc51cca483ef6
UENO, M. <eunosium-1128@live.jp>
2026-05-30 03:19:49 +0900

    Clean up bld.bat by removing commented-out lines
    Removed commented-out code for library copying and directory listing.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,17 +3,13 @@ setlocal enabledelayedexpansion
 
 %PYTHON% %RECIPE_DIR%\make_lcms2_pc.py
 
+@REM Drop Unix-only libm from pkg-config-derived libraries on MSVC
+%PYTHON% %RECIPE_DIR%\patch_pyvips_build.py
+
 copy %LIBRARY_LIB%\bzip2.lib %LIBRARY_LIB%\bz2.lib 
 copy %LIBRARY_LIB%\liblzma.lib %LIBRARY_LIB%\lzma.lib
 copy %LIBRARY_LIB%\libcrypto.lib %LIBRARY_LIB%\crypto.lib
 copy %LIBRARY_LIB%\libssl.lib %LIBRARY_LIB%\ssl.lib
-:: if exist %LIBRARY_LIB%\libarchive.lib copy %LIBRARY_LIB%\libarchive.lib %LIBRARY_LIB%\archive.lib
-:: if exist %LIBRARY_LIB%\zlib.lib copy %LIBRARY_LIB%\zlib.lib %LIBRARY_LIB%\z.lib
-:: if exist %LIBRARY_LIB%\libbz2.lib copy %LIBRARY_LIB%\libbz2.lib %LIBRARY_LIB%\bz2.lib
-:: if exist %LIBRARY_LIB%\liblzma.lib copy %LIBRARY_LIB%\liblzma.lib %LIBRARY_LIB%\lzma.lib
-:: if exist %LIBRARY_LIB%\liblz4.lib copy %LIBRARY_LIB%\liblz4.lib %LIBRARY_LIB%\lz4.lib
-:: if exist %LIBRARY_LIB%\libzstd.lib copy %LIBRARY_LIB%\libzstd.lib %LIBRARY_LIB%\zstd.lib
-dir %LIBRARY_LIB%\*.lib
 
 @REM Ensure we can find the vips package
 pkg-config --exists --print-errors vips
