45408a104b7bee6ac5fdf37c9913bc3615251c81
UENO, M. <eunosium-1128@live.jp>
2026-05-30 02:20:32 +0900

    Update bld.bat to handle bz2.lib copying
    Added conditional copy for bz2.lib if it does not exist.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,13 +3,14 @@ setlocal enabledelayedexpansion
 
 %PYTHON% %RECIPE_DIR%\make_lcms2_pc.py
 
-dir %LIBRARY_LIB%\*.lib
+if not exist %LIBRARY_LIB%\bz2.lib copy %LIBRARY_LIB%\bzip2.lib %LIBRARY_LIB%\bz2.lib 
 :: if exist %LIBRARY_LIB%\libarchive.lib copy %LIBRARY_LIB%\libarchive.lib %LIBRARY_LIB%\archive.lib
 :: if exist %LIBRARY_LIB%\zlib.lib copy %LIBRARY_LIB%\zlib.lib %LIBRARY_LIB%\z.lib
 :: if exist %LIBRARY_LIB%\libbz2.lib copy %LIBRARY_LIB%\libbz2.lib %LIBRARY_LIB%\bz2.lib
 :: if exist %LIBRARY_LIB%\liblzma.lib copy %LIBRARY_LIB%\liblzma.lib %LIBRARY_LIB%\lzma.lib
 :: if exist %LIBRARY_LIB%\liblz4.lib copy %LIBRARY_LIB%\liblz4.lib %LIBRARY_LIB%\lz4.lib
 :: if exist %LIBRARY_LIB%\libzstd.lib copy %LIBRARY_LIB%\libzstd.lib %LIBRARY_LIB%\zstd.lib
+dir %LIBRARY_LIB%\*.lib
 
 @REM Ensure we can find the vips package
 pkg-config --exists --print-errors vips
